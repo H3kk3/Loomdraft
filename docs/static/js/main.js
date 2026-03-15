5edf28d90624f8d13cf2de785af7daa25916c996
Hekke <henri.eloranta90@gmail.com>
2026-03-15 21:31:23 +0200

    Created workflow for build
    - Github release workflow
    - Hugo toml to point to the correct versions
    - Links for both intel and apple silicon builds

--- docs/static/js/main.js
+++ docs/static/js/main.js
@@ -2,8 +2,12 @@
 (function () {
   var ua = navigator.userAgent;
   var os = 'linux';
-  if (ua.indexOf('Mac') !== -1) os = 'mac';
-  else if (ua.indexOf('Win') !== -1) os = 'win';
+  if (ua.indexOf('Mac') !== -1) {
+    // Default to Apple Silicon; Intel users can pick the other button
+    os = 'mac-arm';
+  } else if (ua.indexOf('Win') !== -1) {
+    os = 'win';
+  }
 
   // Highlight the matching download button
   var buttons = document.querySelectorAll('.btn-download[data-os]');
@@ -19,7 +23,12 @@
     var match = document.querySelector('.btn-download[data-os="' + os + '"]');
     if (match) {
       heroBtn.setAttribute('href', match.getAttribute('href'));
-      var osName = { mac: 'macOS', win: 'Windows', linux: 'Linux' };
+      var osName = {
+        'mac-arm': 'macOS (Apple Silicon)',
+        'mac-intel': 'macOS (Intel)',
+        win: 'Windows',
+        linux: 'Linux',
+      };
       heroBtn.querySelector('span').textContent = 'Download for ' + osName[os];
     }
   }
